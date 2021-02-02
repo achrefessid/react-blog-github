cd68d25fc27a634e6f0a4bce889fee5e656b7e28
Achref Essid <essid.developpeur@gmail.com>
2021-02-02 22:16:03 +0100

    first commit

---
+++ src/config.js
@@ -0,0 +1,32 @@
+export const config = {
+  // Your Github Converted Token To Know How To Get Your Token Look at Readme.md
+  githubConvertedToken: "4b1416934b48e5246c5ae4ab74aa1abbc7c6d132 ",
+
+  // Your Github UserName
+  githubUserName: "achrefessid",
+
+  // Your Github Repo Name Where You Have your issues as Blog
+  githubRepo: "react-blog-github",
+
+  // Set it to true if you have a Github app to add to this project
+  // and fill the client ID & secret
+  enableOAuth: true,
+  OAuthClientID: 'b1d5496244b16fffa530',
+  OAuthSecret: '92e3a0beb56dbcd1dda454dee8ea5df539f5fe2f ',
+
+  // Your Personal Blog Title
+  title : "Achref Essid" ,
+
+  // Your Personal Blog Subtitle
+  subtitle : "Software Developer",
+
+  // Header customization
+  header: {
+    backgroundColor: '#f1f6f8', // can be a CSS gradient
+    backgroundColorDark: '#4C566A',
+    titleColor: '#ff5252',
+    titleColorDark: '#16a085',
+    subtitleColor: '#37474f',
+    subtitleColorDark: '#D8DEE9',
+  },
+};
